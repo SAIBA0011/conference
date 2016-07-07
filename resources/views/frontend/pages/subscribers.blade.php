be90eb67921f7df26fcf2bb837225e8ca176b7aa
Tiaan Theunissen <Tiaant@saiba.org.za>
2016-07-07 10:14:22 +0200

    updated site

--- resources/views/frontend/pages/subscribers.blade.php
+++ resources/views/frontend/pages/subscribers.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     <section class="section">
        <div class="container">
-           <div class="col-md-12">
-               <table class="table">
-                   <thead>
+           <div class="row">
+               <div class="col-md-12">
+                   <table class="table">
+                       <thead>
                        <th>Full Name</th>
                        <th>Email</th>
                        <th>Phone</th>
@@ -13,23 +14,24 @@
                        <th>Member Body</th>
                        <th>Professional body</th>
                        <th>Option</th>
-                   </thead>
-                   <tbody>
-                    @if(count($subscribers))
-                        @foreach($subscribers as $subscriber)
-                            <tr>
-                                <td>{{ $subscriber->first_name }} {{ $subscriber->last_name }}</td>
-                                <td>{{ $subscriber->email }}</td>
-                                <td>{{ $subscriber->phone }}</td>
-                                <td>{{ $subscriber->country }}</td>
-                                <td>{{ $subscriber->member_body }}</td>
-                                <td>{{ $subscriber->body_sa }}</td>
-                                <td>{{ $subscriber->option }}</td>
-                            </tr>
-                        @endforeach
-                    @endif
-                   </tbody>
-               </table>
+                       </thead>
+                       <tbody>
+                       @if(count($subscribers))
+                           @foreach($subscribers as $subscriber)
+                               <tr>
+                                   <td>{{ $subscriber->first_name }} {{ $subscriber->last_name }}</td>
+                                   <td>{{ $subscriber->email }}</td>
+                                   <td>{{ $subscriber->phone }}</td>
+                                   <td>{{ $subscriber->country }}</td>
+                                   <td>{{ $subscriber->member_body }}</td>
+                                   <td>{{ $subscriber->body_sa }}</td>
+                                   <td>{{ $subscriber->option }}</td>
+                               </tr>
+                           @endforeach
+                       @endif
+                       </tbody>
+                   </table>
+               </div>
            </div>
        </div>
     </section>
